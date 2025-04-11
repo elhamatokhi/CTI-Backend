ffde50289585601efec5f138ab543f3d353e13b0
Elhama Tokhi <tokhielaham30@gmail.com>
2025-04-11 21:13:33 +0200

    Created a JSON file that keeps the data persistent'

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import path from "path";
+
+//  Initializing express app
 const app = express();
 const PORT = 3000;
 
+// __dirname Setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let movies = [];
 
+//  Read movies from the JSON file when the server restarts
+const moviesFilePath = path.join(__dirname, "movies.json");
+fs.readFile(moviesFilePath, "utf-8", (err, data) => {
+  if (err) {
+    console.error(`Error reading the JSON file: ${err}`);
+  } else movies = JSON.parse(data);
+});
+
+// Create and display movies
 app.post("/submit", (req, res) => {
   const { movie, rating } = req.body;
 
-  movies.push({ movie, rating });
   if (!movie || !rating) {
     return res.status(400).send("<h3>Movie name and rating are required!</h3>");
   }
 
-  res.render("success.ejs", { movie, rating });
+  movies.push({ movie, rating });
+
+  fs.writeFile(moviesFilePath, JSON.stringify(movies, null, 2), (err) => {
+    if (err) {
+      return res.status(500).send("Error writing to the JSON file.");
+    }
+    res.render("success.ejs", { movie, rating });
+  });
 });
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { movies });
 });
 
+// Sever
 app.listen(PORT, () => {
   console.log(`The Server is listening on port ${PORT}`);
 });
