af54a7d16d4385013a9658110588277c26b0799c
Elhama Tokhi <tokhielaham30@gmail.com>
2025-04-11 20:08:20 +0200

    Created the server and /submit route

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
-
+import morgan from "morgan";
+import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
 
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.send("Heyy");
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const { movie, rating } = req.body;
+
+  console.log(req.body);
+  try {
+    res.render("success.ejs", { movie: movie, rating: rating });
+  } catch (error) {
+    res.status(500).send(`<h3>Sever is not responding</h3>`);
+  }
 });
 app.listen(PORT, () => {
   console.log(`The server is listening on port ${PORT}`);
