910c3615d811d6eecc6c46726e74912860ba1ff2
Elhama Tokhi <tokhielaham30@gmail.com>
2025-04-25 21:07:43 +0200

    Add header and footer as EJS partials

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import fs from "fs";
 import path from "path";
 import routes from "./routes/index.js";
+import { nextTick } from "process";
 
 //  Initializing express app
 const app = express();
@@ -15,11 +16,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middlewares
-// app.use(morgan("dev"));
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.use((req,res,next)=>{
+  res.locals.siteName = "Favorite Movies"
+  res.locals.logoPath = "/Images/logo.jpg"
+
+  next()
+})
 // Routes
 app.use("/", routes);
 
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/movies/:name", (req, res) => {
+  const movieName = req.params.name;
+  res.render("movieList", { movieName });
+});
+
 export default router;
