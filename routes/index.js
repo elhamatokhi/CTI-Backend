fec7dab74216c0320be18cf12ab9ac4ae99acaf9
Elhama Tokhi <tokhielaham30@gmail.com>
2025-04-26 11:32:23 +0200

    Used header and footer as partials

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,18 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get('/movieList',(req,res)=>{
+  res.render('movieList')
+})
 router.get("/movies/:name", (req, res) => {
   const movieName = req.params.name;
   res.render("movieList", { movieName });
 });
+
+
+router.post('/submit',(req,res)=>{
+  const {movie, rating} = req.body
+  res.render('success',{movie,rating})
+})
 
 export default router;
