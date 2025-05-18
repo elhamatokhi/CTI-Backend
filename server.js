97056ff46358ac8e1371e7211f13af5c05765ca4
Elhama Tokhi <tokhielaham30@gmail.com>
2025-05-18 13:37:04 +0200

    Removed json from imported files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import bodyParser, { json } from 'body-parser'
+import bodyParser from 'body-parser'
 import _ from 'lodash'
 import router from './routes/index.js'
 const app = express()
